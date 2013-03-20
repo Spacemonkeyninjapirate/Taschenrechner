e4ca7dab677d67f53c0bd5abf8a14dd450c810b3
Sage D. Layton <Goldenerkreiger@hotmail.de>
2013-03-20 02:53:22 +0100

    Variable durch break ersetzt
    ~

--- Taschenrechner/Taschenrechner/main.cpp
+++ Taschenrechner/Taschenrechner/main.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 void main()
 {
-    bool soll_beenden = false;
-
     char *eingabe = new char [EINGABEBUFFER];
 
-    while (soll_beenden != true)
+    while (true)
     {
         cin.getline(eingabe, EINGABEBUFFER);
 
-        soll_beenden = (strncmp(eingabe, "quit", 4) == 0 || strncmp(eingabe, "exit", 4) == 0);
+        if(strncmp(eingabe, "quit", 4) == 0 || strncmp(eingabe, "exit", 4) == 0)
+        {
+            break;
+        }
     }
 
     delete[] eingabe;
