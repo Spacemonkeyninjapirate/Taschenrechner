1afa87149b1107260ded21083ee21ea589fd6ab9
Sage D. Layton <Goldenerkreiger@hotmail.de>
2013-03-20 03:59:50 +0100

    Erkennung von Ziffern und Operatoren

--- Taschenrechner/Taschenrechner/main.cpp
+++ Taschenrechner/Taschenrechner/main.cpp
@@ -8,15 +8,34 @@ using namespace std;
 void main()
 {
     char *eingabe = new char [EINGABEBUFFER];
+    char *rest = eingabe;
 
     while (true)
     {
         cin.getline(eingabe, EINGABEBUFFER);
+        int i;
 
         if(strncmp(eingabe, "quit", 4) == 0 || strncmp(eingabe, "exit", 4) == 0)
         {
             break;
         }
+
+            while(*rest != 0)
+            {
+                if(isdigit(*rest))
+                {
+                for(i = 0; isdigit(rest[i]) != false; i++);
+                atol(rest);
+                rest = &rest[i];
+                }
+                else if (*rest == '+' || *rest == '-' || *rest == '/' || *rest == '*')
+                {
+                }
+                else
+                {
+                rest = &rest[1];
+                }
+            }
     }
 
     delete[] eingabe;
